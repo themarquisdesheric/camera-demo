ad0eb0e2c6652df046204fc8ddfcaad1c2c603aa
Yuval Allweil <themarquisdesheric@gmail.com>
2018-04-09 13:13:15 -0700

    basic video working desktop

---
+++ app.js
@@ -0,0 +1,14 @@
+const video = document.querySelector('video');
+const constraints = {
+    audio: false,
+    video: {
+        facingMode: 'user'
+    }
+}
+
+navigator.mediaDevices.getUserMedia(constraints)
+  .then(stream => video.srcObject = stream)
+  .catch(err => {
+      console.log(err);
+      alert(err);
+  });
